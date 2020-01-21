a4433c4f2a652799c8859377782575bee9d8ec3c
Sharath <sharathpavan7@gmail.com>
2020-01-22 00:15:00 +0530

    commit - v0.0.1 service module is added. includes model & repository classes.

---
+++ app/src/main/java/com/sharath/mvvmgooglegitprojects/service/repository/ProjectRepository.java
@@ -0,0 +1,83 @@
+package com.sharath.mvvmgooglegitprojects.service.repository;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.google.gson.Gson;
+import com.sharath.mvvmgooglegitprojects.service.model.Project;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ProjectRepository {
+    private static ProjectRepository projectRepository;
+    private GithubService githubService;
+
+    private ProjectRepository() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(GithubService.HTTPS_API_GITHUB_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        githubService = retrofit.create(GithubService.class);
+    }
+
+    public synchronized static ProjectRepository getInstance() {
+        if (projectRepository == null) {
+            projectRepository = new ProjectRepository();
+        }
+        return projectRepository;
+    }
+
+    public LiveData<List<Project>> getProjectList(String userId) {
+        final MutableLiveData<List<Project>> data = new MutableLiveData<>();
+
+        githubService.getProjectList(userId).enqueue(new Callback<List<Project>>() {
+            @Override
+            public void onResponse(Call<List<Project>> call, Response<List<Project>> response) {
+                data.setValue(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<List<Project>> call, Throwable t) {
+                // TODO better error handling in part #2 ...
+                data.setValue(null);
+            }
+        });
+
+        return data;
+    }
+
+    public LiveData<Project> getProjectDetails(String userID, String projectName) {
+        final MutableLiveData<Project> data = new MutableLiveData<>();
+
+        githubService.getProjectDetails(userID, projectName).enqueue(new Callback<Project>() {
+            @Override
+            public void onResponse(Call<Project> call, Response<Project> response) {
+                simulateDelay();
+                data.setValue(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<Project> call, Throwable t) {
+                // TODO better error handling in part #2 ...
+                data.setValue(null);
+            }
+        });
+
+        return data;
+    }
+
+    private void simulateDelay() {
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
