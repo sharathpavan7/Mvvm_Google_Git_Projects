05eb00261b0fd68a9870d4cfc53264803dfe4398
Sharath <sharathpavan7@gmail.com>
2020-03-08 11:42:10 +0530

    commit - v0.0.1 Project list adaper codeing complete.

--- app/src/main/java/com/sharath/mvvmgooglegitprojects/view/adapter/ProjectListAdapter.java
+++ app/src/main/java/com/sharath/mvvmgooglegitprojects/view/adapter/ProjectListAdapter.java
@@ -6,6 +6,8 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.MutableLiveData;
+import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.sharath.mvvmgooglegitprojects.R;
@@ -13,6 +15,7 @@ import com.sharath.mvvmgooglegitprojects.databinding.ProjectListItemBinding;
 import com.sharath.mvvmgooglegitprojects.services.model.Project;
 
 import java.util.List;
+import java.util.Objects;
 
 public class ProjectListAdapter extends RecyclerView.Adapter<ProjectListAdapter.ProjectListViewHolder>{
 
@@ -33,12 +36,13 @@ public class ProjectListAdapter extends RecyclerView.Adapter<ProjectListAdapter.
 
     @Override
     public void onBindViewHolder(@NonNull ProjectListViewHolder holder, int position) {
-
+        holder.binding.setProject(projectList.get(position));
+        holder.binding.executePendingBindings();
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return projectList == null? 0 : projectList.size();
     }
 
     class ProjectListViewHolder extends RecyclerView.ViewHolder{
@@ -47,5 +51,40 @@ public class ProjectListAdapter extends RecyclerView.Adapter<ProjectListAdapter.
             super(binding.getRoot());
             this.binding = binding;
         }
+    }
+
+    public void setProjectList(final List<Project> projectList) {
+        if (projectList == null) {
+            this.projectList = projectList;
+            notifyItemRangeInserted(0, projectList.size());
+        } else {
+            DiffUtil.DiffResult result = DiffUtil.calculateDiff(new DiffUtil.Callback() {
+                @Override
+                public int getOldListSize() {
+                    return ProjectListAdapter.this.projectList.size();
+                }
+
+                @Override
+                public int getNewListSize() {
+                    return projectList.size();
+                }
+
+                @Override
+                public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
+                    return ProjectListAdapter.this.projectList.get(oldItemPosition).id ==
+                            projectList.get(newItemPosition).id;
+                }
+
+                @Override
+                public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
+                    Project project = projectList.get(newItemPosition);
+                    Project old = ProjectListAdapter.this.projectList.get(oldItemPosition);
+                    return project.id == old.id && Objects.equals(project.git_url, old.git_url);
+                }
+            });
+            this.projectList = projectList;
+            result.dispatchUpdatesTo(this);
+        }
+
     }
 }
